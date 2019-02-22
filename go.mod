fe164fcd8218aa9393f99d61ec3cc0677245f043
Brady Sullivan <brady@bsull.com>
2019-02-21 16:31:41 -0800

    Initial honeypot code.

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/d1str0/Drupot
+
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/d1str0/hpfeeds v0.1.1
+)
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/BurntSushi/toml"
+	"github.com/d1str0/hpfeeds"
+)
+
+type Config struct {
+	StartPort     int
+	EndPort       int
+	ExcludedPorts []int
+	HpfConfig     *HpfConfig `toml:"hpfeeds"`
+}
+
+// Config for Hpfeeds publishing
+type HpfConfig struct {
+	Host    string
+	Port    int
+	Ident   string
+	Auth    string
+	Channel string
+}
+
+func main() {
+	fmt.Println("///- Running WhiteNoise")
+
+	// Load config file
+	var configFilename string
+	var logFilename string
+	flag.StringVar(&configFilename, "c", "config.toml", "load given config file")
+	flag.StringVar(&logFilename, "l", "", "log file to write to")
+
+	flag.Parse()
+
+	fmt.Printf("//- Loading config file: %s\n", configFilename)
+	c := loadConfig(configFilename)
+	if c.HpfConfig != nil {
+		hpc := c.HpfConfig
+		fmt.Printf("/- Connecting to hpfeeds server: %s\n", hpc.Host)
+		fmt.Printf("/-\tPort: %d\n", hpc.Port)
+		fmt.Printf("/-\tIdent: %s\n", hpc.Ident)
+		fmt.Printf("/-\tAuth: %s\n", hpc.Auth)
+		fmt.Printf("/-\tChannel: %s\n", hpc.Channel)
+
+		client := hpfeeds.NewClient(hpc.Host, hpc.Port, hpc.Ident, hpc.Auth)
+		err := client.Connect()
+		if err != nil {
+			log.Fatalf("Error connecting to hpfeeds server: %s\n", err.Error())
+		}
+
+		publish := make(chan []byte)
+		client.Publish(hpc.Channel, publish)
+		publish <- []byte("test")
+	}
+
+	if logFilename != "" {
+		fmt.Printf("//- Logging to file: %s\n", logFilename)
+	}
+
+}
+
+func loadConfig(filename string) *Config {
+	var c Config
+	_, err := toml.DecodeFile(filename, &c)
+	if err != nil {
+		log.Fatalf("Unable to parse config file: %s\n", err.Error())
+	}
+	return &c
+}
